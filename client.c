ea9a5adf0554191e4875a6cf38765a5507bda81b
Danny De Novi <70696078+dannydenovi@users.noreply.github.com>
2023-04-14 14:25:29 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -1,3 +1,12 @@
+//crea socket
+//set porta e ip
+// manda richiesta al server
+
+//invia messaggio
+// ricevere risposta del server
+// chiude connessione
+
+
 //Import headerfiles
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,60 +15,47 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <pthread.h>
-
-void* routine(void* arg);
 
 int main() {
-    long int i = 0;
-    pthread_t threads[30];
-    
-    while (i<30){
-        pthread_create(&threads[i], NULL, routine, (void*)(i+1));
-        usleep(1000);
-        i++;
-    }
-    
-    return 0;
-}
-
-void* routine(void* arg) {
-
-    long int i = (long int) arg;
     char serverMessage[2000], clientMessage[2000];
+    int serverDescriptor;
     struct sockaddr_in serverAddr;
-    int serverDescriptor; 
+
+    memset(serverMessage, '\0', sizeof(serverMessage));
+    memset(clientMessage, '\0', sizeof(clientMessage));
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(2002);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     serverDescriptor = socket(AF_INET, SOCK_STREAM, 0);
 
     if(serverDescriptor < 0){
         printf("ERROR: could not create socket.");
-        pthread_exit(NULL);
+        return -1;
     }
     
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7799);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
     if(connect(serverDescriptor, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0){
-        printf("\nERROR: connection error.");
-        pthread_exit(NULL);
+        printf("ERROR: connection error.");
+        return -1;
     }
 
-    sprintf(clientMessage, "Hello from client %ld\n", i);
+    printf("Enter message: ");
+    fgets(clientMessage, sizeof(clientMessage), stdin);
 
+    
     if(send(serverDescriptor, clientMessage, strlen(clientMessage), 0) < 0){
         printf("ERROR: could not send message.");
-        pthread_exit(NULL);
+        return -1;
     }
 
     if(recv(serverDescriptor, serverMessage, sizeof(serverMessage), 0) < 0){
         printf("ERROR: could not receive message.");
-        pthread_exit(NULL);
+        return -1;
     }
 
     printf("\nServer: %s\n", serverMessage);
     close(serverDescriptor);
-    return NULL;
+
+    return 0;
 }
