1806d2bbfd33fce60259d4b3fca57d265a64f882
Danny De Novi <70696078+dannydenovi@users.noreply.github.com>
2023-04-14 14:58:22 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -1,13 +1,3 @@
-//crea socket
-//set porta e ip
-// manda richiesta al server
-
-//invia messaggio
-// ricevere risposta del server
-// chiude connessione
-
-
-//Import headerfiles
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -15,47 +5,61 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+
+#define CLIENT_LIMIT 30
+
+void* routine(void* arg);
 
 int main() {
+    long int i = 0; 
+    pthread_t threads[CLIENT_LIMIT]; 
+    
+    while (i<CLIENT_LIMIT){
+        pthread_create(&threads[i], NULL, routine, (void*)(i+1));
+        usleep(500);
+        i++;
+    }
+    
+    return 0;
+}
+
+void* routine(void* arg) {
+
+    long int i = (long int) arg;
     char serverMessage[2000], clientMessage[2000];
-    int serverDescriptor;
     struct sockaddr_in serverAddr;
-
-    memset(serverMessage, '\0', sizeof(serverMessage));
-    memset(clientMessage, '\0', sizeof(clientMessage));
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(2002);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int serverDescriptor; 
 
     serverDescriptor = socket(AF_INET, SOCK_STREAM, 0);
 
     if(serverDescriptor < 0){
         printf("ERROR: could not create socket.");
-        return -1;
+        pthread_exit(NULL);
     }
     
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(7799);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
     if(connect(serverDescriptor, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0){
-        printf("ERROR: connection error.");
-        return -1;
+        printf("\nERROR: connection error.");
+        pthread_exit(NULL);
     }
 
-    printf("Enter message: ");
-    fgets(clientMessage, sizeof(clientMessage), stdin);
+    sprintf(clientMessage, "Hello from client %li\n", i);
 
-    
     if(send(serverDescriptor, clientMessage, strlen(clientMessage), 0) < 0){
         printf("ERROR: could not send message.");
-        return -1;
+        pthread_exit(NULL);
     }
 
     if(recv(serverDescriptor, serverMessage, sizeof(serverMessage), 0) < 0){
         printf("ERROR: could not receive message.");
-        return -1;
+        pthread_exit(NULL);
     }
 
     printf("\nServer: %s\n", serverMessage);
     close(serverDescriptor);
-
-    return 0;
+    return NULL;
 }
--- server.c
+++ server.c
@@ -1,68 +1,106 @@
-//Import headerfiles
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main() {
+#define true 1
+#define PORT 7799
+#define LOCALHOST "127.0.0.1"
+#define CLIENT_LIMIT 30
+#define BUFFER_SIZE 2000
 
-    char senderMessage[2000], recvMessage[2000];
+FILE *fp;
+struct Args{
+    int arg1;
+    pthread_mutex_t *arg2;
+};
+
+void *routine(void *arg);
+
+int main(){
+
     int socket_descriptor, client_sock, client_size;
     struct sockaddr_in server_addr, client_addr;
+    int i = 0;
+    pthread_t threads[30];
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-    //pulizia del buffer memset()
-    memset(senderMessage, '\0', sizeof(senderMessage));
-    memset(recvMessage, '\0', sizeof(recvMessage));
-
-    // Creazione socket socket()
     socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(socket_descriptor < 0){
+    if (socket_descriptor < 0){
         printf("ERROR: Could not create socket.");
         return -1;
     }
 
-    //set port and ip
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2002);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(LOCALHOST);
 
-    //bind port and ip
-    bind(socket_descriptor,  (struct sockaddr*) &server_addr, sizeof(server_addr));
-    
-    // listen for clients listen()
-    if(listen(socket_descriptor, 1) < 0){
+    bind(socket_descriptor, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    if (listen(socket_descriptor, CLIENT_LIMIT) < 0){
         printf("ERROR: could not listen for incoming connections.");
         return -1;
     }
 
-    client_size = sizeof(client_addr);
-    //accept incoming connections
-    client_sock = accept(socket_descriptor, (struct sockaddr*) &client_addr, &client_size);
-
-    if(client_sock < 0){
-        printf("ERROR: could not accept connections.");
-        return -1;
+    while (true){
+        client_size = sizeof(client_addr);
+        struct Args args = {client_sock, &mutex};
+        client_sock = accept(socket_descriptor, (struct sockaddr *)&client_addr, (socklen_t *)&client_size);
+        pthread_create(&threads[i], NULL, routine, (void *)&args);
+        i == CLIENT_LIMIT ? i = 0 : i++;
     }
-
-    if(recv(client_sock, recvMessage, sizeof(recvMessage), 0) < 0){
-        printf("ERROR: could not receive message.");
-        return -1;
-    }
-
-    strcpy(senderMessage, "Ciao dal server");
-
-    if(send(client_sock, senderMessage, sizeof(senderMessage), 0) < 0){
-        printf("ERROR: could not send message.");
-        return -1;
-    }
-    printf("\n%s", recvMessage);
 
     close(socket_descriptor);
+    return 0;
+}
+
+void *routine(void *arg){
+
+    struct Args *args = (struct Args *)arg;
+    int client_sock = args->arg1;
+    pthread_mutex_t *mutex = args->arg2;
+
+    char senderMessage[BUFFER_SIZE], recvMessage[BUFFER_SIZE];
+    memset(senderMessage, '\0', sizeof(senderMessage));
+    memset(recvMessage, '\0', sizeof(recvMessage));
+
+    if (client_sock < 0){
+        printf("ERROR: could not accept connections.");
+        pthread_exit(NULL);
+    }
+
+    if (recv(client_sock, recvMessage, sizeof(recvMessage), 0) < 0){
+        printf("ERROR: could not receive message.");
+        pthread_exit(NULL);
+    }
+
+    strcpy(senderMessage, "Hello world!");
+
+    if (send(client_sock, senderMessage, sizeof(senderMessage), 0) < 0){
+        printf("ERROR: could not send message.");
+        pthread_exit(NULL);
+    }
+
+    printf("\n%s", recvMessage);
     close(client_sock);
 
-    return 0;
+    pthread_mutex_lock(mutex);
+    fp = fopen("file.txt", "a+");
+
+    if (fp == NULL){
+        printf("ERROR: could not open file.");
+        pthread_mutex_unlock(mutex);
+        pthread_exit(NULL);
+    }
+
+    fputs(recvMessage, fp);
+    fclose(fp);
+    pthread_mutex_unlock(mutex);
+
+    return NULL;
 }
